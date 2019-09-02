aa477c700d68d32157221283480cceff5c57a182
xiaoqi <308805548@qq.com>
2019-09-02 10:34:12 +0800

    反转字符串

---
+++ leetcode/344-反转字符串/1-反转字符串.js
@@ -0,0 +1,15 @@
+function func (s) {
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+  console.log(s);
+}
+
+func(["h","e","l","l","o"]);
