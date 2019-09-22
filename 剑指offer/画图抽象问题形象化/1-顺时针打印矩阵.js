6d3b7abb44616be26fbc33e587bda64bcea97be0
xiaoqi <308805548@qq.com>
2019-09-22 11:33:16 +0800

    顺时针打印矩阵

--- 剑指offer/画图抽象问题形象化/1-顺时针打印矩阵.js
+++ 剑指offer/画图抽象问题形象化/1-顺时针打印矩阵.js
@@ -1,43 +1,50 @@
-function printMatrix(matrix)
+/*
+ * @Description: 
+ * @Author: xiaoqi
+ * @Date: 2019-08-26 12:26:43
+ * @LastEditTime: 2019-09-20 21:26:28
+ * @LastEditors: xiaoqi
+ */
+function printMatrix(arr)
 {
   // write code here
   let res = [];
-  if (!matrix.length) {
+  if (!arr.length) {
     return;
   }
   // 一维矩阵
-  if (!matrix[0].length) {
-    for (let i = 0; i < matrix.length; i++) {
-      // console.log(matrix[i]);
-      res.push(matrix[i]);
+  if (!arr[0].length) {
+    for (let i = 0; i < arr.length; i++) {
+      // console.log(arr[i]);
+      res.push(arr[i]);
     }
     return res;
   }
   
   // 存左上角和右下角
   let topLeft = [0, 0];
-  let bottomRight = [matrix.length - 1, matrix[0].length - 1];
+  let bottomRight = [arr.length - 1, arr[0].length - 1];
 
   while (topLeft[0] < bottomRight[0] && topLeft[1] < bottomRight[1]) {
     // 第一行
     for (let i = topLeft[1]; i <= bottomRight[1]; i++) {
-      // console.log(matrix[topLeft[0]][i]);
-      res.push(matrix[topLeft[0]][i]);
+      // console.log(arr[topLeft[0]][i]);
+      res.push(arr[topLeft[0]][i]);
     }
     // 最后一列
     for (let j = topLeft[0] + 1; j <= bottomRight[0]; j++) {
-      // console.log(matrix[j][bottomRight[1]]);
-      res.push(matrix[j][bottomRight[1]]);
+      // console.log(arr[j][bottomRight[1]]);
+      res.push(arr[j][bottomRight[1]]);
     }
     // 最后一行
     for (let i = bottomRight[1] - 1; i >= topLeft[1]; i--) {
-      // console.log(matrix[bottomRight[0]][i]);
-      res.push(matrix[bottomRight[0]][i]);
+      // console.log(arr[bottomRight[0]][i]);
+      res.push(arr[bottomRight[0]][i]);
     }
     // 第一列
     for (let j = bottomRight[0] - 1; j >= topLeft[0] + 1; j--) {
-      // console.log(matrix[j][topLeft[1]]);
-      res.push(matrix[j][topLeft[1]]);
+      // console.log(arr[j][topLeft[1]]);
+      res.push(arr[j][topLeft[1]]);
     }
     topLeft[0] += 1;
     topLeft[1] += 1;
@@ -46,19 +53,19 @@ function printMatrix(matrix)
   }
   // // 特殊情况，奇数正方形，或者非方形
   if (topLeft[0] === bottomRight[0] && topLeft[1] === bottomRight[1]) {
-    // console.log(matrix[topLeft[0]][topLeft[1]]);
-    res.push(matrix[topLeft[0]][topLeft[1]]);
+    // console.log(arr[topLeft[0]][topLeft[1]]);
+    res.push(arr[topLeft[0]][topLeft[1]]);
     return res;
   } else if (topLeft[0] === bottomRight[0] && topLeft[1] < bottomRight[1]) {
-    // console.log(matrix[topLeft[0]][topLeft[1]]);
-    // console.log(matrix[bottomRight[0]][bottomRight[1]]);
+    // console.log(arr[topLeft[0]][topLeft[1]]);
+    // console.log(arr[bottomRight[0]][bottomRight[1]]);
     for (let i = topLeft[1]; i <= bottomRight[1]; i++) {
-      res.push(matrix[topLeft[0]][i]);
+      res.push(arr[topLeft[0]][i]);
     }
     return res;
   } else if (topLeft[1] === bottomRight[1] && topLeft[0] < bottomRight[0]) {
     for (let j = topLeft[0]; j <= bottomRight[0]; j++) {
-      res.push(matrix[j][topLeft[1]]);
+      res.push(arr[j][topLeft[1]]);
     }
     return res;
   }
@@ -66,22 +73,22 @@ function printMatrix(matrix)
 }
 
 
-// let matrix = [[1, 2, 3], 
+// let arr = [[1, 2, 3], 
 //               [4, 5, 6], 
 //               [7, 8, 9]];
-// let matrix = [[1, 2, 3, 4]];
-let matrix = [[1], 
-              [4], 
-              [7]];
-// let matrix = [[1, 2, 3, 4],
+// let arr = [[1, 2, 3, 4]];
+// let arr = [[1], 
+//               [4], 
+//               [7]];
+// let arr = [[1, 2, 3, 4],
 //               [5, 6, 7, 8],
 //               [9, 10, 11, 12],
 //               [13, 14, 15, 16]];
-// let matrix = [1, 2, 3, 4];
-// let matrix = [];
-// let matrix = [[1, 2, 3, 4, 5],
-//               [6, 7, 8, 9, 10],
-//               [11, 12, 13, 14, 15]];
+// let arr = [1, 2, 3, 4];
+// let arr = [];
+let arr = [[1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10],
+              [11, 12, 13, 14, 15]];
 
-let res = printMatrix(matrix);
+let res = printMatrix(arr);
 console.log(res);
